2c4e857cb9bd4d8d79707c7d9d3aecd1fadb259f
Jacks1002 <133033178+Jacks1002@users.noreply.github.com>
2025-06-03 19:19:18 +0800

    test1

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-print("HelloWorld!")
-print("Good from Super Starrr!")
+import os
+
+def ctext(text,cname):
+    color = {
+        'gray': 90,
+        'red': 91,
+        'green': 92,
+        'yellow': 93,
+        'blue': 94,
+        'purple': 95,
+        'cyan': 96 
+    }
+    return f"\033[{color[cname]}{text}\033[0m"
+
+def main():
+    if os.name != 'nt':
+        input("\033[91;1mStarTool support Windows system only!")
+        exit()
+    os.system('cls')
+    print(ctext('StarTool','blue'),'-',ctext('Your best tool in CWK.','cyan'))
+    
+    
